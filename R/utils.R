c6fbb198a82af074f04c5f96194422533bb2e43d
Cynthia Crowley <cmc559@cornell.edu>
2024-01-03 10:52:17 -0500

    Six-part edit
    1/6 Add functions
    dat2sf()
    get_date_quantiles()
    date_ecdf()
    
    2/6 Edit formula2terms
    To use mgcv::interpret.gam. Seems to cover all use cases thus far.
    
    3/6 Edit plot_var_imp_rf to:
    i. optionally change facet scales and variable names
    Ii. Plot a label geom that shows the OOB performance of the model
    Iii. Use cmc.utils::ggtheme() in the output
    
    4/6 Tweak ggtheme  font sizes and families.
    
    5/6 Add a bunch of weighting functions (might want to relegate to a different file in the near future)
    
    6/6 Add partial dependence calculations (dunno if we need these; I was doing these mostly as a learning exercise)

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,11 @@
+#' Convert data frame to sf object with ESPG 4326 SRD
+#' @export
+dat2sf <- function(dat, coord_cols=c('longitude', 'latitude')){
+  dat %>%
+    sf::st_as_sf(coords=coord_cols) %>%
+    sf::st_set_crs("+proj=longlat +datum=WGS84")
+}
+
 #' @name get_missing_dates
 #' @description Given a vector of dates, return any that are 'skipped', ie missing. Assumes neither the first nor the last date in the sequence is missing.
 #' @export
@@ -10,18 +18,55 @@ get_missing_dates <- function(dates){
   return(seqd[!(seqd %in% dates)])
 }
 
+#' get quantiles on vector of dates
+#'
+#' @param dates Date vector
+#' @param probs numeric; quantiles for which you want exceedance probabilities
+#'
+#' @export
+get_date_quantiles <- function(dates, probs){
+  as.Date(quantile(as.numeric(dates), probs), origin="1970-01-01")
+}
+
+#' Convert a vector of dates to values on [0,1]
+#' @param dates; Date
+#'
+#' @export
+date_ecdf <- function(dates){
+  n <- as.numeric(dates)
+  cdf <- ecdf(n)
+  cdf(n)
+}
+
 #' @description Found at: https://stackoverflow.com/questions/520810/does-r-have-quote-like-operators-like-perls-qw
 #' @export
 names2chr <- function(...){
   sapply(match.call()[-1], deparse)
 }
 
+#' Parse individual variable names from formula terms
+#'
+#' (intended to handle GAM arguments like s(foo, bs="re", "k=10"))
+#'
+terms2vars <- function(terms){
+  sapply(terms, function(term){
+    stringr::str_replace_all(terms,
+                         c("s\\("= "",
+                           ",\\s*[a-z]+\\s*=\\s*.+\\s*\\)$"="")
+    ) %>%
+      stringr::str_split(pattern="\\s*,\\s*") %>%
+      unlist()
+  })
+
+}
 
 #' @name formula2terms
 #' @param formula `formula` or `character`
 #' @export
 formula2terms <- function(formula){
-  return(rownames(attr(terms.formula(as.formula(formula)), "factors")))
+  # return(rownames(attr(terms.formula(as.formula(formula)), "factors")))
+  f.gam <- mgcv:::interpret.gam(formula)
+  return(c(f.gam$pred.names, f.gam$response))
 }
 
 #' Get the number of out-of-bag trees for each data point
@@ -56,13 +101,16 @@ get_var_imp_rf <- function(rf){
 #' @param rf_list list of `ranger` model objects
 #' @param n_top (int) subset of variables for which to plot importances
 #' @export
-plot_var_imp_rf <- function(rf_list, n_top=NULL){
+plot_var_imp_rf <- function(rf_list, n_top=NULL, facet_scales="fixed", names_function=NULL){
   # do.call(rbind, lapply(rf_list, function(rf){}))
   stopifnot(is.list(rf_list))
 
   df_all <- do.call(rbind, lapply(seq_along(rf_list), function(i){
     df <- get_var_imp_rf(rf_list[[i]])
     df$rf_name <- names(rf_list)[i]
+    df$r2_oob <- ifelse('r.squared' %in% names(rf_list[[i]]),
+                        round(rf_list[[i]]$r.squared, 3),
+                        round(1-rf_list[[i]]$prediction.error, 3))
     return(df)
   }))
 
@@ -70,11 +118,13 @@ plot_var_imp_rf <- function(rf_list, n_top=NULL){
     df_all$rf_name <- factor(df_all$rf_name, levels=names(rf_list), ordered=TRUE)
   }
 
-
-  # Compute the mean importance across all rfs to determine facet ordering
+  if(!is.null(names_function)){
+    df_all$var_name <- names_function(df_all$var_name)
+  }
+  # Compute the mean importance across all rfs to determine variable ordering
   df_all <- df_all %>%
     dplyr::group_by(var_name) %>%
-    dplyr::mutate(mean_imp = mean(importance_value)) %>%
+    dplyr::mutate(mean_imp = mean(importance_value, na.rm=TRUE)) %>%
     dplyr::ungroup() %>%
     dplyr::mutate(var_name=factor(var_name, levels=unique(var_name[order(mean_imp)])))
 
@@ -86,16 +136,21 @@ plot_var_imp_rf <- function(rf_list, n_top=NULL){
   vars_include <- vars_include[1:n_top] %>% droplevels()
   df_sub <- df_all %>% dplyr::filter(var_name %in% vars_include) %>% droplevels()
 
-  p <- ggplot2::ggplot(df_sub, ggplot2::aes(x=round(importance_value), y=var_name)) +
-    ggplot2::geom_dotplot(binaxis = "y", ggplot2::aes(fill=var_name, color=var_name)) +
-    ggplot2::facet_wrap(~rf_name, nrow=2) +
-    ggplot2::theme(legend.position='bottom') +
+  labels <- df_sub %>% distinct(rf_name, .keep_all=TRUE) %>% select(rf_name, r2_oob)
+  max_imp <- max(df_sub$importance_value)
+  p <- ggplot2::ggplot() +
+    ggplot2::geom_dotplot(data=df_sub, binaxis = "y",
+                          ggplot2::aes(x=round(importance_value), y=var_name,
+                                       fill=var_name, color=var_name)) +
+    ggplot2::geom_text(data=labels, ggplot2::aes(label=sprintf("R2=\n %s", as.character(r2_oob))),
+                       x=0.8*max_imp,
+                       y=3) +
+    ggplot2::facet_wrap(~rf_name, nrow=2, scales=facet_scales) +
     ggplot2::labs(y='Variable',
-         x='Importance Value',
-         title= "RF importances by tixel",
-         subtitle="for top 10 mean importance across tixels")
+         x='Importance Value')
 
-  return(p)
+  return(ggtheme(p, scale_x_date=FALSE) + ggplot2::theme(legend.position='none')
+)
 }
 
 
@@ -183,30 +238,37 @@ ggtheme <- function(p, scale_x_date=TRUE, text_multiplier = 1){
     p <- p + ggplot2::scale_x_date(date_breaks='1 year',
                                    date_labels="'%y")
   }
-  p + ggplot2::theme(axis.title=ggplot2::element_text(size=14*text_multiplier,
+  p + ggplot2::theme(axis.title=ggplot2::element_text(size=15*text_multiplier,
                                                       color=text_col,
                                                       family="Helvetica"#,
                                                       #face='bold'
   ),
-  axis.text.y=ggplot2::element_text(size=13*text_multiplier,
+  axis.text.y=ggplot2::element_text(size=14*text_multiplier,
                                     color=text_col,
                                     family="Helvetica"),
-  axis.text.x=ggplot2::element_text(size=9*text_multiplier,
+  axis.text.x=ggplot2::element_text(size=14*text_multiplier,
                                     color=text_col,
                                     family="Helvetica"),
   plot.title=ggplot2::element_text(family="Helvetica",
                                    face="bold",
                                    color=text_col,
                                    size=20*text_multiplier,
-                                   hjust=0),
+                                   hjust=0,
+                                   # top,right,bottom,left
+                                   margin=ggplot2::margin(0,0,5,0)),
   plot.subtitle=ggplot2::element_text(family="Helvetica",
                                       color=text_col,
                                       hjust=0,
-                                      size=15*text_multiplier,
+                                      size=16*text_multiplier,
                                       margin=ggplot2::margin(t=4, b=7)),
-  strip.text.x = ggplot2::element_text(size=13.5*text_multiplier, hjust=0,
+  strip.text.x = ggplot2::element_text(size=14*text_multiplier, hjust=0,
                                        color=text_col,
                                        family="Helvetica"),
+  strip.text.y = ggplot2::element_text(size=14*text_multiplier,
+                                       color=text_col,
+                                       family="Helvetica",
+                                       angle=-90,
+                                       hjust=0.5),
   strip.background = ggplot2::element_rect(fill=NA,#'grey95',
                                            color=NA),
   panel.background = ggplot2::element_rect(fill='white', color=NA),
@@ -214,9 +276,9 @@ ggtheme <- function(p, scale_x_date=TRUE, text_multiplier = 1){
   panel.grid=ggplot2::element_line(color='grey92'),
   legend.key = ggplot2::element_rect(fill='white', color=NA),
   legend.text=ggplot2::element_text(color=text_col,
-                                    size=13*text_multiplier),
+                                    size=14*text_multiplier),
   legend.title=ggplot2::element_text(color=text_col,
-                                     size=14*text_multiplier),
+                                     size=15*text_multiplier),
   complete=TRUE)
 }
 
@@ -245,6 +307,129 @@ read_db_table <- function(path_db){
 
 
 #' @export
-get_date_from_doy <- function(year, doy){
+doy2date <- function(year, doy){
   as.Date(doy -1, origin=sprintf('%d-01-01', year))
+}
+
+
+
+##### caseweighting functions
+#' Create weights inversely proportional to relative class size
+#'
+#' @param class_col character vector of classes
+#' @export
+stratify2weights <- function(class_col){
+  tab <- prop.table(table(class_col))
+
+  return((1-sapply(class_col, function(class){tab[[class]]})) / min(tab))
+}
+
+#' @export
+joint_caseweights <- function(dat, stratify_col, caseweight_col){
+  message_time("Computing joint case weights of ", caseweight_col, " stratified by ", stratify_col)
+  stopifnot(length(stratify_col) == 1 & length(caseweight_col) == 1)
+  stratify_ws <- stratify2weights(dat[[stratify_col]])
+  final_weights <- dat[[caseweight_col]]*stratify_ws
+  return(final_weights)
+}
+
+#' Calculate an "effective" sample size when using weights
+#'
+#' @export
+n_effective <- function(w){
+  sum(w)^2 / sum(w^2)
+}
+
+#' Calculate a weighted mean and a standard error of the mean in multiple ways
+#'
+#' @export
+weighted_mean_se <- function(x, obs_ss, type='2', cap_ss_value=NULL){
+  # x = valid cci estimates of length n_folds
+  # obs_ss = observer sample size that went into that estimate
+  # type 1 true weighted mean, where we calculate an effective sample size based on the weights
+  # type 2 each point gets mostly the same weight, but the uncertainties on each are very different
+
+
+  ####################
+  # Create the weights
+  ####################
+  #The optimal allocation of weights is to make each point's weight inversely proportional to the square of its
+  # uncertainty
+  # ie, in inverse proportion to its variance
+  #(lower sample size =higher uncertainty = lower weight)
+  # Create weights that are capped if specified
+  obs_ss <- obs_ss[!is.na(x)]
+  x <- x[!is.na(x)]
+  if(!is.null(cap_ss_value)){
+    obs_ss[obs_ss > cap_ss_value] <- cap_ss_value
+  }
+  #observe the measured uncertainties
+  # w = 1/variance --> variance = 1/weight
+  # Assuming CCI is in and of itself a mean of IID normal variables (z-scores)
+  # (which is not quite true at least because there's correlation, not independence)
+  # -- to deal with this, change sqrt(n) in the denominator to something smaller
+  # then we should assume the variance decreases as the sqrt(n) increases
+  w <- sqrt(obs_ss)
+
+  ################
+  # Sample sizes
+  ################
+  # I think we should use type 2 but we can try both
+  if(type=='1'){
+    n = n_effective(w) # basically dealing across folds and weighted-summing the sample sizes within folds
+  } else if(type=='2'){
+    n = length(x[!is.na(x)]) # counting only the valid folds
+  } else{
+    stop('Must specify type "1" or "2"')
+  }
+  correction = n / (n-1)
+
+  xbar <- stats::weighted.mean(x, w)
+  variance_i <- correction*(sum( w*( (x-xbar)^2 ) / sum(w) ))
+  variance_mean <- variance_i / n
+  return(list(
+    cci_estimate=xbar,
+    cci_se = sqrt(variance_mean)
+  )
+  )
+}
+
+
+
+# Partial dependence function (without the summarize step)
+# TODO: make it so that there's a flexible response
+# logit vs prob vs class
+# currently, can only have one grid sise even if there are multiple variables to jointly conduct PD
+#' @export
+ice <- function(dat, mod, vars, grid_size){
+  stopifnot( all(vars %in% names(dat)) )
+  datmod <- dat %>% dplyr::select(-all_of(vars))
+
+  # get the artificial vectors of variables of interest to compute over:
+  vals <- lapply(vars, function(var){
+    seq(from=min(dat[[var]], na.rm=TRUE),
+        to=max(dat[[var]], na.rm=TRUE),
+        length.out = grid_size)
+  })
+  # What's not clear to me: Does PD(x, y) mean that we compute over the combination of X and Y
+  # For the moment: do the exhuastive combos while waiting for an answer on S.O.
+  combos <- expand.grid(vals)
+  names(combos) <- vars
+
+  message("Generating data over combinations of vars...")
+  pd_dat <- do.call(rbind, lapply(1:nrow(combos), function(i){
+    d <- datmod
+    for(j in 1:length(vars)){
+      vn <- vars[j]
+      d[[vn]] <- combos[[vn]][j]
+    }
+    return(d)
+  }))
+
+  # TODO: use arg predict_fun if we wanto to not be specific to ranger?
+  message('Computing predictions...')
+  preds <- cbind(combos, predict(mod, pd_dat)$predictions)
+  names(preds) <- c(names(combos), sprintf('prob_%s', mod$forest$class.values))
+
+  return(list('ice'=preds))
 }
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -2,10 +2,8 @@ test_that("formula2terms", {
   f1 <- y ~ x1 + x2*x3
   f2 <- resid ~ s(checklist_number, observer_id, bs="fs")
 
-  expect_equal(formula2terms(f1), c('y', 'x1', 'x2', 'x3'))
-
-  #TODO see mgcv::interpret.gam
-  expect_equal(formula2terms(f2), c('checklist_number', 'observer_id'))
+  expect_setequal(formula2terms(f1), c('y', 'x1', 'x2', 'x3'))
+  expect_setequal(formula2terms(f2), c('resid', 'checklist_number', 'observer_id'))
 })
 
 
@@ -16,5 +14,5 @@ test_that('get_missing_dates errors on non-date class', {
 
 
 test_that('names2chr',{
-  expect_equal(names2chr(foo, bar, buzz), c("foo", "bar", "buzz"))
+  expect_setequal(names2chr(foo, bar, buzz), c("foo", "bar", "buzz"))
 })
